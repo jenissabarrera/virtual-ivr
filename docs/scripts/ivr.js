a9d50be4fa0a494e49f41ea59bd45e14abcb11fe
jenissabarrera <Jenissa.Barrera@genesys.com>
2019-09-30 17:57:03 +0800

    generate hierarchy of the flow

--- docs/scripts/ivr.js
+++ docs/scripts/ivr.js
@@ -11,14 +11,15 @@ const architectScripting = require('purecloud-flow-scripting-api-sdk-javascript'
 // Helpers to make sample code more readable.
 // --------------------------------------------------------------------------------
 const scriptingActionFactory = architectScripting.factories.archFactoryActions; // Factory to create actions
-const scriptingEnums         = architectScripting.enums.archEnums;              // Enum support
 const scriptingFlowFactory   = architectScripting.factories.archFactoryFlows;   // Factory to create flows
 const scriptingLanguages     = architectScripting.languages.archLanguages;      // Language support
 const scriptingSession       = architectScripting.environment.archSession;      // Session support
 const scriptingTaskFactory   = architectScripting.factories.archFactoryTasks;   // Factory to create tasks
 const scriptingLogger        = architectScripting.services.archLogging;         // Logging support
+
 const archEnums              = architectScripting.enums.archEnums;
-const scriptingmenu          = architectScripting.viewModels.menus;
+
+
 
 
 scriptingLogger.logNotesVerbose = true;
@@ -28,7 +29,7 @@ const clientId1 = '751ad006-d414-44af-9787-f1f511bd9487';
 const clientSecret = 'CXZG6tmDOUm-aQDLJHp7VBU2c2X5IvPgcr461VyLwTc'
 
 // Declare global variable
-let flowid = "";
+let flows = {};
 
 // --------------------------------------------------------------------------------
 // This function will be what is called by Architect Scripting since it is
@@ -37,34 +38,53 @@ let flowid = "";
 function getFlowTypes() {
 
   let flowType = archEnums.FLOW_TYPES;
-  console.log('testing flow type' + JSON.stringify(flowType));
-
   return flowType;
   
 }
 
 
-function authenticateCredentials(){
+function authenticateCredentials(flowType){
+        // Pass object from http
+        flows = flowType;
 
-        scriptingSession.startWithClientIdAndSecret(archEnums.LOCATIONS.prod_us_east_1, getFlowTypes1, clientId1, clientSecret);
-
+        scriptingSession.endTerminatesProcess = false;
+        scriptingSession.startWithClientIdAndSecret(archEnums.LOCATIONS.prod_us_east_1, openFlow, clientId1, clientSecret);
 }
 
 function openFlow(scriptSession){
-    
-      scriptingFlowFactory.getFlowInfoByFlowIdAsync(flowid, scriptingEnums.FLOW_TYPES.inboundCall,function(ArchBaseFlowInfoBasic) {
+
+      let flowType = flows.flowInfoFlowType;
+      
+  //     scriptingFlowFactory.getFlowInfoByFlowIdAsync(flows.flowid, flowType.toLowerCase(),function(ArchBaseFlowInfoBasic) {
+  //     console.log('------------------------------------------------------')
+  //     console.log('LOGGING INFO STRING:' + ArchBaseFlowInfoBasic.parentflow)
+  //     console.log('------------------------------------------------------')
+
+
+  // }) 
+
+      scriptingFlowFactory.checkoutAndLoadFlowByFlowIdAsync(flows.flowid, flowType.toLowerCase(),false,"latest",function(callbackArchFlowInfoBasic) {
       console.log('------------------------------------------------------')
-      console.log('THIS IS NAME:' + ArchBaseFlowInfoBasic.name)
-      console.log('------------------------------------------------------')
+     // console.log('LOGGING INFO STRING:' + JSON.stringify(callbackArchFlowInfoBasic.callbackTraverseInfo ));
+      
+      // let sample = ArchBaseCoreObjectWithId.logStr;
+
+      callbackArchFlowInfoBasic.traverse(function(traverseInfo) {
+        scriptingLogger.logNote('------------------------------------------------------')  
+        scriptingLogger.logNote('  Object     : ' + traverseInfo.matchObject.logStr); 
+        scriptingLogger.logNote('  Type Name     : ' + traverseInfo.matchObject.displayTypeName);
+        scriptingLogger.logNote('    Hierarchy: ' + traverseInfo.context.hierarchyStr);
+        scriptingLogger.logNote("\n");
+
+        console.log('------------------------------------------------------')  
+     });
 
 
 
 
-  }) 
+  })
   
 }
-
-
 
 
 module.exports = {
