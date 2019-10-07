33191a6d4431672176a42f08c0a8352b8f7fd4c2
jenissabarrera <Jenissa.Barrera@genesys.com>
2019-10-07 14:29:35 +0800

    package json

--- docs/scripts/ivr.js
+++ docs/scripts/ivr.js
@@ -1,4 +1,3 @@
-
 // --------------------------------------------------------------------------------
 // Require in the PureCloud Platform SDK and Architect Scripting SDK
 // --------------------------------------------------------------------------------
@@ -11,18 +10,21 @@ const architectScripting = require('purecloud-flow-scripting-api-sdk-javascript'
 // Helpers to make sample code more readable.
 // --------------------------------------------------------------------------------
 const scriptingActionFactory = architectScripting.factories.archFactoryActions; // Factory to create actions
-const scriptingFlowFactory   = architectScripting.factories.archFactoryFlows;   // Factory to create flows
-const scriptingLanguages     = architectScripting.languages.archLanguages;      // Language support
-const scriptingSession       = architectScripting.environment.archSession;      // Session support
-const scriptingTaskFactory   = architectScripting.factories.archFactoryTasks;   // Factory to create tasks
-const scriptingLogger        = architectScripting.services.archLogging;         // Logging support
+const scriptingFlowFactory = architectScripting.factories.archFactoryFlows; // Factory to create flows
+const scriptingLanguages = architectScripting.languages.archLanguages; // Language support
+const scriptingSession = architectScripting.environment.archSession; // Session support
+const scriptingTaskFactory = architectScripting.factories.archFactoryTasks; // Factory to create tasks
+const scriptingLogger = architectScripting.services.archLogging; // Logging support
+const scriptingValues = architectScripting.viewModels.values;
 
-const archEnums              = architectScripting.enums.archEnums;
+const archEnums = architectScripting.enums.archEnums;
+
+const scriptingBaseClasses = architectScripting.viewModels.baseClasses;
 
 
 
 
-scriptingLogger.logNotesVerbose = true;
+scriptingLogger.logNotesVerbose = false;
 
 // Declare clientId and clientSecret for 
 const clientId1 = '751ad006-d414-44af-9787-f1f511bd9487';
@@ -39,22 +41,22 @@ function getFlowTypes() {
 
   let flowType = archEnums.FLOW_TYPES;
   return flowType;
-  
+
 }
 
 
-function authenticateCredentials(flowType){
-        // Pass object from http
-        flows = flowType;
+function authenticateCredentials(flowType) {
+  // Pass object from http
+  flows = flowType;
 
-        scriptingSession.endTerminatesProcess = false;
-        scriptingSession.startWithClientIdAndSecret(archEnums.LOCATIONS.prod_us_east_1, openFlow, clientId1, clientSecret);
+  scriptingSession.endTerminatesProcess = false;
+  scriptingSession.startWithClientIdAndSecret(archEnums.LOCATIONS.prod_us_east_1, openFlow, clientId1, clientSecret);
 }
 
-function openFlow(scriptSession){
+function openFlow(scriptSession) {
 
-      let flowType = flows.flowInfoFlowType;
-      
+  let flowType = flows.flowInfoFlowType;
+
   //     scriptingFlowFactory.getFlowInfoByFlowIdAsync(flows.flowid, flowType.toLowerCase(),function(ArchBaseFlowInfoBasic) {
   //     console.log('------------------------------------------------------')
   //     console.log('LOGGING INFO STRING:' + ArchBaseFlowInfoBasic.parentflow)
@@ -63,33 +65,66 @@ function openFlow(scriptSession){
 
   // }) 
 
-      scriptingFlowFactory.checkoutAndLoadFlowByFlowIdAsync(flows.flowid, flowType.toLowerCase(),false,"latest",function(callbackArchFlowInfoBasic) {
+
+  console.log("Start here!")
+  scriptingFlowFactory.checkoutAndLoadFlowByFlowIdAsync(flows.flowid, flowType.toLowerCase(), false, "latest", function (callbackArchFlowInfoBasic) {
+    console.log('------------------------------------------------------')
+    // console.log('LOGGING INFO STRING:' + JSON.stringify(callbackArchFlowInfoBasic.callbackTraverseInfo ));
+
+    // let sample = ArchBaseCoreObjectWithId.logStr;
+
+    callbackArchFlowInfoBasic.initialAudio
+
+    callbackArchFlowInfoBasic.traverse(function (traverseInfo) {
+      console.log("------------------------------------------------------")
+
+      // scriptingLogger.logNote('------------------------------------------------------')  
+      // scriptingLogger.logNote('  Object     : ' + traverseInfo.matchObject.logStr); 
+      // scriptingLogger.logNote('  Type Name     : ' + traverseInfo.matchObject.displayTypeName);
+      // scriptingLogger.logNote('    Hierarchy: ' + traverseInfo.context.hierarchyStr);
+      // scriptingLogger.logNote("\n");
+      // map.set('Object:', traverseInfo.matchObject.logStr);
+      // map.set('Type Name', traverseInfo.matchObject.displayTypeName);
+      // map.set('Hierarchy', traverseInfo.context.hierarchy);
+
+
+      console.log('  Object     : ' + traverseInfo.matchObject.logStr);
+      console.log('  Type Name     : ' + traverseInfo.matchObject.displayTypeName);
+      console.log('    Hierarchy: ' + JSON.stringify(traverseInfo.context.hierarchyStr));
+      console.log("\n");
       console.log('------------------------------------------------------')
-     // console.log('LOGGING INFO STRING:' + JSON.stringify(callbackArchFlowInfoBasic.callbackTraverseInfo ));
-      
-      // let sample = ArchBaseCoreObjectWithId.logStr;
 
-      callbackArchFlowInfoBasic.traverse(function(traverseInfo) {
-        scriptingLogger.logNote('------------------------------------------------------')  
-        scriptingLogger.logNote('  Object     : ' + traverseInfo.matchObject.logStr); 
-        scriptingLogger.logNote('  Type Name     : ' + traverseInfo.matchObject.displayTypeName);
-        scriptingLogger.logNote('    Hierarchy: ' + traverseInfo.context.hierarchyStr);
-        scriptingLogger.logNote("\n");
+      if(traverseInfo.matchObject.isArchValueAudio){
+        test(traverseInfo.matchObject);
+      }
+    });
 
-        console.log('------------------------------------------------------')  
-     });
-
-
+ 
 
 
   })
   
+
+}
+
+function test(audioObject) {
+  console.log(audioObject.getExpressionText());
+  console.log(audioObject.getVariable());
+
+  // to do
+//   scriptingValues.ArchValueAudio(function (getExpressionText){
+
+// )
+//   })
+  // console.log("dtmf:" + scriptingBaseClasses.ArchBaseMenuChoice.isArchBaseMenuChoice )
+
 }
 
 
+
 module.exports = {
-  
-    authenticateCredentials: authenticateCredentials,
-    getFlowTypes : getFlowTypes
+
+  authenticateCredentials: authenticateCredentials,
+  getFlowTypes: getFlowTypes
 
 };
